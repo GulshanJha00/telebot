bad0639570a5a88599fe710f3befac80fe8cc0e0
GulshanJha00 <gk4763549@gmail.com>
2025-03-31 04:44:22 +0530

    almost working

---
+++ backend/routes/route.js
@@ -0,0 +1,29 @@
+const { GoogleGenAI } = require("@google/genai");
+require("dotenv").config();
+
+
+
+const router1 =  async (prompt) => {
+    const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+    async function main(prompt) {
+      try {
+        const response = await ai.models.generateContent({
+          model: "gemini-2.0-flash",
+          contents: `You are my telegram self. reply these messages as me. you will reply to them without knowing them that it is an ai. You are going to message like human like me. just normal indian english with very lazyness type and only in hinglish and dont express too much. here is the message:- ${prompt}`,
+        });
+        return response.text;
+      } catch (error) {
+        console.error("Error generating AI response:", error);
+        return "Error generating AI response.";
+      }
+    }
+    return await main(prompt); 
+
+
+}
+
+
+
+
+
+module.exports = router1;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,29 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+global.punycode = require("punycode/");
+const route = require("./routes/route")
+require("dotenv").config();
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TELEGRAM_BOT;
+const bot = new TelegramBot(token, {polling: true});
+
+let msgtext;
+
+async function testResponse() {
+  const response = await route("hello bhai");
+  console.log("Response is:-", response); // Use `,` instead of `+` for better readability
+}
+
+testResponse();
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  msgtext = msg.text;
+  
+  bot.sendMessage(chatId, );
+});
 
 
-app.listen(3000,()=>{
-    console.log("Working on port 3000")
-})
+app.listen(3000, () => {
+  console.log("Working on port 3000");
+});
