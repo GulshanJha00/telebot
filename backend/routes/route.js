4a76140c50367cd1615367c9c555fc14fa4bc716
GulshanJha00 <gk4763549@gmail.com>
2025-03-31 05:12:14 +0530

    working good

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -9,7 +9,7 @@ const router1 =  async (prompt) => {
       try {
         const response = await ai.models.generateContent({
           model: "gemini-2.0-flash",
-          contents: `You are my telegram self. reply these messages as me. you will reply to them without knowing them that it is an ai. You are going to message like human like me. just normal indian english with very lazyness type and only in hinglish and dont express too much. here is the message:- ${prompt}`,
+          contents: `You're name is gulshan and you are my telegram self. reply these messages as me. you will reply to them without knowing them that it is an ai. You are going to message like human like me. just normal indian english with very lazyness type and only in hinglish and dont express too much. here is the message:- ${prompt}`,
         });
         return response.text;
       } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-global.punycode = require("punycode/");
 const route = require("./routes/route")
 require("dotenv").config();
 const TelegramBot = require('node-telegram-bot-api');
@@ -8,19 +7,12 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_BOT;
 const bot = new TelegramBot(token, {polling: true});
 
-let msgtext;
 
-async function testResponse() {
-  const response = await route("hello bhai");
-  console.log("Response is:-", response); // Use `,` instead of `+` for better readability
-}
-
-testResponse();
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
-  msgtext = msg.text;
-  
-  bot.sendMessage(chatId, );
+  const response = await route(msg.text);
+  console.log("Response is:-", response);
+  bot.sendMessage(chatId, response);
 });
 
 
